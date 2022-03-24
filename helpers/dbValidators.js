fc1fd07b31c333868089c4b25a11670c1a15a7dd
Miguel <chanampamiguel@gmail.com>
2022-03-24 16:39:40 -0300

    User - Validaciones

---
+++ helpers/dbValidators.js
@@ -0,0 +1,22 @@
+const User = require('../models/users');
+
+const usernameExists = async ( username = '' ) => {
+    const user = await User.findOne( { where: { username } }) ;
+    
+    if ( user ) {
+        throw new Error(`El nombre de usuario '${ username }' ya está registrado en la base de datos.`);
+    };
+};
+
+const emailExists = async ( email = '' ) => {
+    const user = await User.findOne( { where: { email } }) ;
+    
+    if ( user ) {
+        throw new Error(`El email '${ email }' ya está registrado en la base de datos.`);
+    };
+};
+
+module.exports = {
+    usernameExists,
+    emailExists
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,18 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { register } = require('../controllers/auth');
+const { usernameExists, emailExists } = require('../helpers/dbValidators');
+const { validateFields } = require('../middlewares/validateFields');
 
 const router = Router();
 
-router.post('/register', register);
+router.post('/register', [
+    check('username', 'El nombre de usuario es obligatorio').not().isEmpty(),
+    check('username').custom( usernameExists ),
+    check('email', 'El email no es válido').isEmail(),
+    check('email').custom( emailExists ),
+    check('password', 'La contraseña debe tener mínimo 8 caracteres, un número, una letra mayúscula y una letra minúscula').isStrongPassword({ minSymbols: 0 }),
+    validateFields
+], register);
 
 module.exports = router;
