c14ae6abf432d5285f0a0716a25bc65b2ac42c36
Miguel <chanampamiguel@gmail.com>
2022-03-24 20:11:33 -0300

    Characters - Listado de personajes

--- controllers/characters.js
+++ controllers/characters.js
@@ -1,5 +1,15 @@
 const { response } = require('express');
+const Character = require('../models/character');
+
+const getCharacters = async ( req, res = response ) => {
+    
+    const characters = await Character.findAll();
+
+    res.json({
+        characters
+    });
+};
 
 module.exports = {
-
+    getCharacters
 };
