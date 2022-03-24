e9f04343a409f5bdad474b4183287a6edd75416a
Miguel <chanampamiguel@gmail.com>
2022-03-24 20:33:17 -0300

    Character - Crear personaje

--- controllers/characters.js
+++ controllers/characters.js
@@ -2,7 +2,7 @@ const { response } = require('express');
 const Character = require('../models/character');
 
 const getCharacters = async ( req, res = response ) => {
-    
+
     const characters = await Character.findAll();
 
     res.json({
@@ -10,6 +10,17 @@ const getCharacters = async ( req, res = response ) => {
     });
 };
 
+const createCharacter = async ( req, res = response ) => {
+    
+    const character = await Character.create( req.body );
+
+    res.status(200).json({
+        msg: 'El personaje se ha creado con éxito',
+        character
+    });
+};
+
 module.exports = {
-    getCharacters
+    getCharacters,
+    createCharacter
 };
--- routes/characters.js
+++ routes/characters.js
@@ -1,9 +1,20 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { validateJWT } = require('../middlewares/validateJWT');
-const { getCharacters } = require('../controllers/characters');
+const { validateFields } = require('../middlewares/validateFields');
+const { getCharacters, createCharacter } = require('../controllers/characters');
 
 const router = Router();
 
 router.get('/', validateJWT, getCharacters);
+
+router.post('/', [
+    validateJWT,
+    check('name', 'El nombre del personaje es obligatorio').not().isEmpty(),
+    check('age', 'La edad del personaje no es válida').isInt(),
+    check('weight', 'El peso del personaje no es válido').isNumeric(),
+    check('history', 'La historia del personaje es obligatoria').not().isEmpty(),
+    validateFields
+], createCharacter);
 
 module.exports = router;
