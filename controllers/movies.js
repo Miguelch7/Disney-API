ea104cbe7ca5e3ca6e04b7b999f9d866b2d83256
Miguel <chanampamiguel@gmail.com>
2022-03-25 11:50:50 -0300

    Movie - Crear Película/Serie

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,7 +1,17 @@
 const { response } = require('express');
 const Movie = require('../models/movie');
 
+// Crear película/serie
+const createMovie = async ( req, res = response ) => {
+    
+    const movie = await Movie.create( req.body );
+
+    res.status(200).json({
+        msg: 'La película/serie se ha creado con éxito',
+        movie
+    });
+};
 
 module.exports = {
-    
+    createMovie
 };
--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,18 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { validateJWT } = require('../middlewares/validateJWT');
+const { validateFields } = require('../middlewares/validateFields');
+const { 
+    createMovie, 
+} = require('../controllers/movies');
 
 const router = Router();
+
+// Crear película/serie
+router.post('/', [
+    validateJWT,
+    check('title', 'El título de la película/serie es obligatorio').not().isEmpty(),
+    validateFields
+], createMovie);
 
 module.exports = router;
